596b5b14b7b152881a5fd7b44a58dc24fac3c2c2
maximalekseenko <max.alekseenko@gmail.com>
2023-03-07 13:22:36 +0300

    tile expend

--- tile.h
+++ tile.h
@@ -1,6 +1,12 @@
 #pragma once
 
+int TILE_DIM = 3;
+
 class Tile{
     public:
-        
+        char tile_chars[3][3];
+        int type_up;
+        int type_left;
+        int type_right;
+        int type_down;
 };
