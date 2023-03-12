f1e5117ece2a7c79bee15ce6afd6ecdba3bf2cc7
maximalekseenko <max.alekseenko@gmail.com>
2023-03-12 11:32:21 +0300

    sh build in code folder

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash/env bash
-BUILD_DIR=./build/
-SRC_DIR=./src/
-DATA_DIR=./src/data/
+BASE_DIR=$(dirname "$0")
+BUILD_DIR=$BASE_DIR/build/
+CODE_DIR=$BASE_DIR/src/code/
+DATA_DIR=$BASE_DIR/src/data/
+
 
 # make build directory
 if [ ! -d $BUILD_DIR ]; then
     mkdir $BUILD_DIR
 fi
 
-# compile c++
-g++ -std=c++20 -o $BUILD_DIR/build $SRC_DIR/*.cpp
+
+# xxd -i $DATA_DIR/*.png > $BUILD_DIR/data.
+
+# # compile c++
+g++ -std=c++20 -o $BUILD_DIR/build $CODE_DIR/*.cpp
 
 # add data
-cp -a $DATA_DIR $BUILD_DIR/data
+# cat $BUILD_DIR/build $DATA_DIR/*.PNG > $BUILD_DIR/build2
+# cp -a $DATA_DIR $BUILD_DIR/data
 
-$BUILD_DIR/build
+# run
+(cd $BUILD_DIR; ./build)
